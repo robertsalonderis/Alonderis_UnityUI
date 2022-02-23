704421a7c1c453b23e827432343556bc9b331020
Roberts <robertsalonderis@gmail.com>
2022-02-23 14:58:55 +0200

    Unity Alonderis_UnityUI projekta teksta attēlošana

---
+++ Alonderis_UnityUI/Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+		
+	}
+	
+	public string teksts;
+
+	public GameObject ievadesLauks;
+
+	public GameObject tekstaAttelosana;
+
+	public void uzglabaTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text = "Sveiks "+teksts.ToUpper()+"!";
+	}
+}
